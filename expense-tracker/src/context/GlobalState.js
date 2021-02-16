8a542d7d14accdaa01de35b9853774516148008a
Vidit Shah <viditjs@Vidits-MBP.attlocal.net>
2021-02-15 23:15:19 -0500

    update

---
+++ expense-tracker/src/context/GlobalState.js
@@ -0,0 +1,32 @@
+import React, {useContext, useReducer} from 'react';
+import AppReducer from '../context/AppReducer';
+
+const initialState = {
+    transactions: [
+        { id: 1, text: 'Flower', amount: -20 },
+        { id: 2, text: 'Salary', amount: 300 },
+        { id: 3, text: 'Book', amount: -10 },
+        { id: 4, text: 'Camera', amount: 150 }
+    ]
+};
+
+
+export const GlobalContext = new useContext(initialState);
+
+//provider component
+export const GlobalProvider = ({children}) => { 
+    const [state, dispatch] = useReducer(AppReducer, initialState);
+    //Action
+    // function deleteTransaction(id) {
+    //     dispatch({
+    //         type: 'DELETE_TRANSACTION',
+    //         payload: id
+    //     })
+    // }
+    return(<GlobalContext.Provider value={{
+        transactions: state.transactions,
+        // deleteTransaction
+    }}>
+        {children}
+        </GlobalContext.Provider>);
+}
